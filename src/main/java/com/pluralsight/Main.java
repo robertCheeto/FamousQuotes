1a4168fc293618487bf3b4c2778d52be166c6460
Daniel Spezialetti <dspezialetti@my.yearupunited.org>
2025-10-06 10:10:43 -0400

    * added boolean and while loop to main * added try and catch

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner keyboard = new Scanner(System.in);
+        boolean run = true;
         String[] quotes = {"An apple a day keeps the doctor away if you throw it hard enough",
                 "Mind Bottling", "Rocket Surgery", "This Ain't my first Pony Ride",
                 "We'll burn that bridge when we get to it",
@@ -12,9 +13,20 @@ public class Main {
                 "Bingle Barrel baby", "It looks could kill", "Survival of the Fitness",
                 "Is that you John Wayne? Is this me?"};
 
-        System.out.print("Choose a number between 1-10 for a quote: ");
-        int index = keyboard.nextInt();
-        System.out.println(quotes[index - 1]);
+        while (run) {
+            try {
+                System.out.print("Choose a number between 1-10 for a quote: ");
+                int index = keyboard.nextInt();
+
+                System.out.print(quotes[index - 1]);
+                run = false;
+            }
+            catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+
+        }
+
 
     }
 }
